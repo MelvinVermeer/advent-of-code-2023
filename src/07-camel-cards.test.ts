2e8e65c9000dfca52721ed030e7de31e427a7bd3
Melvin Vermeer <melvin.vermeer@gmail.com>
2023-12-11 23:46:14 +0100

    day 7 part one, dont knwo whats wrong

--- src/07-camel-cards.test.ts
+++ src/07-camel-cards.test.ts
@@ -4,15 +4,26 @@ import { readFileSync } from "fs";
 const data = readFileSync("src/data/07", "utf8").split("\n");
 
 describe("07 - Camel Cards", () => {
-  const sample = [""];
+  const sample = [
+    "32T3K 765",
+    "T55J5 684",
+    "KK677 28",
+    "KTJJT 220",
+    "QQQJA 483",
+  ];
 
   describe("Part 1", () => {
     it("Sample", () => {
-      expect(part1(sample)).toEqual(sample);
+      expect(part1(sample)).toEqual(6440);
     });
 
-    xit("Answer", () => {
-      expect(part1(data)).toEqual(data);
+    it("Answer", () => {
+      expect(part1(data)).toBeLessThan(249104619);
+    });
+
+    it("Answer", () => {
+      expect(part1(data)).toBeLessThan(249062749);
+      console.log(part1(data));
     });
   });
 
--- src/07-camel-cards.ts
+++ src/07-camel-cards.ts
@@ -1,5 +1,78 @@
-export const part1 = (data: any): any => {
-  return data;
+import { sum } from "./shared/math";
+
+const CARD_RANKS = [
+  "A",
+  "K",
+  "Q",
+  "J",
+  "T",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+];
+
+const hasNofAKind = (hand: string[], n: number): boolean => {
+  const counts = new Map<string, number>();
+  for (const card of hand) {
+    const count = counts.get(card) ?? 0;
+    counts.set(card, count + 1);
+  }
+  return Array.from(counts.values()).some((count) => count === n);
+};
+
+const hasFiveOfAKind = (hand: string[]): boolean => hasNofAKind(hand, 5);
+const hasFourOfAKind = (hand: string[]): boolean => hasNofAKind(hand, 4);
+const hasFullHouse = (hand: string[]): boolean =>
+  hasNofAKind(hand, 3) && hasNofAKind(hand, 2);
+const hasThreeOfAKind = (hand: string[]): boolean => hasNofAKind(hand, 3);
+const hasTwoOfAKind = (hand: string[]): boolean => hasNofAKind(hand, 2);
+const hasOneOfAKind = (hand: string[]): boolean => hasNofAKind(hand, 1);
+
+const rules = [
+  hasFiveOfAKind,
+  hasFourOfAKind,
+  hasFullHouse,
+  hasThreeOfAKind,
+  hasTwoOfAKind,
+  hasOneOfAKind,
+];
+
+const bestHand = (handA: string[], handB: string[]): number => {
+  const rankA = rules.findIndex((rule) => rule(handA));
+  const rankB = rules.findIndex((rule) => rule(handB));
+
+  if (rankA !== rankB) {
+    return rankB - rankA;
+  }
+
+  for (let i = 0; i < handA.length; i++) {
+    const rankA = CARD_RANKS.indexOf(handA[i]);
+    const rankB = CARD_RANKS.indexOf(handB[i]);
+
+    if (rankA !== rankB) {
+      return rankB - rankA;
+    }
+  }
+
+  return 0;
+};
+
+const parse = (line: string): [string[], number] => {
+  const [hand, bidString] = line.split(" ");
+  return [hand.split(""), Number(bidString)];
+};
+
+export const part1 = (data: string[]): number => {
+  const cards = data
+    .map(parse)
+    .sort(([handA], [handB]) => bestHand(handA, handB));
+
+  return cards.map(([, bid], rank) => bid * (rank + 1)).reduce(sum, 0);
 };
 
 export const part2 = (data: any): any => {
