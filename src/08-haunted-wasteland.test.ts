7c57a1c437222e3af4b4dc1eee4516f58c75dc49
Melvin Vermeer <melvin.vermeer@gmail.com>
2023-12-11 21:33:31 +0100

    day 08 part 1

--- src/08-haunted-wasteland.test.ts
+++ src/08-haunted-wasteland.test.ts
@@ -4,15 +4,37 @@ import { readFileSync } from "fs";
 const data = readFileSync("src/data/08", "utf8").split("\n");
 
 describe("08 - Haunted Wasteland", () => {
-  const sample = [""];
+  const sample = [
+    "RL",
+    "",
+    "AAA = (BBB, CCC)",
+    "BBB = (DDD, EEE)",
+    "CCC = (ZZZ, GGG)",
+    "DDD = (DDD, DDD)",
+    "EEE = (EEE, EEE)",
+    "GGG = (GGG, GGG)",
+    "ZZZ = (ZZZ, ZZZ)",
+  ];
+
+  const sample2 = [
+    "LLR",
+    "",
+    "AAA = (BBB, BBB)",
+    "BBB = (AAA, ZZZ)",
+    "ZZZ = (ZZZ, ZZZ)",
+  ];
 
   describe("Part 1", () => {
     it("Sample", () => {
-      expect(part1(sample)).toEqual(sample);
+      expect(part1(sample)).toEqual(2);
     });
 
-    xit("Answer", () => {
-      expect(part1(data)).toEqual(data);
+    it("Sample", () => {
+      expect(part1(sample2)).toEqual(6);
+    });
+
+    it("Answer", () => {
+      expect(part1(data)).toEqual(19783);
     });
   });
 
--- src/08-haunted-wasteland.ts
+++ src/08-haunted-wasteland.ts
@@ -1,5 +1,29 @@
-export const part1 = (data: any): any => {
-  return data;
+export const part1 = (data: string[]): number => {
+  const [instructions, _, ...rest] = data;
+
+  let location = "AAA";
+  let steps = 0;
+  const map = rest.reduce((acc, cur) => {
+    const [key, value] = cur.split(" = ");
+    const [L, R] = value.replace("(", "").replace(")", "").split(", ");
+    return {
+      ...acc,
+      [key]: { L, R },
+    };
+  }, {});
+
+  while (location !== "ZZZ") {
+    for (const instruction of instructions.split("")) {
+      steps++;
+      // @ts-ignore
+      location = map[location][instruction];
+      if (location === "ZZZ") {
+        break;
+      }
+    }
+  }
+
+  return steps;
 };
 
 export const part2 = (data: any): any => {
