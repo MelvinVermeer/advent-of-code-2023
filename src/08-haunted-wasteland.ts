75f8bbe5896093da9c3d0c7b9ba545c847ab74b0
Melvin Vermeer <melvin.vermeer@gmail.com>
2023-12-11 22:43:44 +0100

    cleanup day 8

--- src/08-haunted-wasteland.ts
+++ src/08-haunted-wasteland.ts
@@ -1,11 +1,14 @@
 import { lcm } from "./shared/math";
 
-export const part1 = (data: string[]): number => {
-  const [instructions, _, ...rest] = data;
+type Instruction = "L" | "R";
+type Map = {
+  [key: string]: {
+    [direction in Instruction]: string;
+  };
+};
 
-  let location = "AAA";
-  let steps = 0;
-  const map = rest.reduce((acc, cur) => {
+const createMap = (data: string[]): Map =>
+  data.reduce((acc, cur) => {
     const [key, value] = cur.split(" = ");
     const [L, R] = value.replace("(", "").replace(")", "").split(", ");
     return {
@@ -14,10 +17,15 @@ export const part1 = (data: string[]): number => {
     };
   }, {});
 
+export const part1 = (data: string[]): number => {
+  const [instructions, _, ...rest] = data;
+  const map = createMap(rest);
+  let location = "AAA";
+  let steps = 0;
+
   while (location !== "ZZZ") {
-    for (const instruction of instructions.split("")) {
+    for (const instruction of instructions.split("") as Instruction[]) {
       steps++;
-      // @ts-ignore
       location = map[location][instruction];
       if (location === "ZZZ") {
         break;
@@ -30,23 +38,16 @@ export const part1 = (data: string[]): number => {
 
 export const part2 = (data: string[]): any => {
   const [instructions, _, ...rest] = data;
+  const map = createMap(rest);
 
   let steps = 0;
-  const map = rest.reduce((acc, cur) => {
-    const [key, value] = cur.split(" = ");
-    const [L, R] = value.replace("(", "").replace(")", "").split(", ");
-    return {
-      ...acc,
-      [key]: { L, R },
-    };
-  }, {});
+
   let locations = Object.keys(map).filter((key) => key.endsWith("A"));
   let solutions = Array(locations.length).fill(0);
 
   while (!solutions.every((solution) => solution !== 0)) {
-    for (const instruction of instructions.split("")) {
+    for (const instruction of instructions.split("") as Instruction[]) {
       steps++;
-      // @ts-ignore
       locations = locations.map((location) => map[location][instruction]);
 
       for (let i = 0; i < locations.length; i++) {
@@ -54,11 +55,12 @@ export const part2 = (data: string[]): any => {
           solutions[i] = steps;
         }
       }
+
       if (solutions.every((solution) => solution !== 0)) {
         break;
       }
     }
   }
 
-  return solutions.reduce(lcm, 1);
+  return solutions.reduce(lcm);
 };
