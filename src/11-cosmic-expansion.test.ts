d320633960403e979e0bbbe86b0a87d655e5c5d3
Melvin Vermeer <melvin.vermeer@gmail.com>
2023-12-11 21:33:36 +0100

    11

---
+++ src/11-cosmic-expansion.test.ts
@@ -0,0 +1,28 @@
+import { part1, part2 } from "./11-cosmic-expansion";
+import { readFileSync } from "fs";
+
+const data = readFileSync("src/data/11", "utf8").split("\n");
+
+describe("11 - Cosmic Expansion", () => {
+  const sample = [""];
+
+  describe("Part 1", () => {
+    it("Sample", () => {
+      expect(part1(sample)).toEqual(sample);
+    });
+
+    xit("Answer", () => {
+      expect(part1(data)).toEqual(data);
+    });
+  });
+
+  describe("Part 2", () => {
+    it("Sample", () => {
+      expect(part2(sample)).toEqual(sample);
+    });
+
+    xit("Answer", () => {
+      expect(part2(data)).toEqual(data);
+    });
+  });
+});
